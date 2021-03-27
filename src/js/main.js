77173fb3f3159c1256c5d333cea154ed4f2e11fc
RamonBomfim <ramon.fbomfim@users.noreply.github.com>
2021-03-26 23:21:23 -0300

    Curso finalizado
    Anotações de coisas pontuais que aprendi.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,57 @@
-var nome = "Ramon Bomfim"
-var idade = 26
+// Aula 1
+// var nome = "Ramon Bomfim"
+// var idade = 26
 
-alert("Bem vindo " + nome + ", " + idade + " anos")
-console.log(nome.replace("Bomfim", "Paulo"))
+// alert("Bem vindo " + nome + ", " + idade + " anos") // Apresenta um popup ao iniciar a página
+// console.log(nome.replace("Bomfim", "Paulo")) // Imprime o resultado no console e troca a parte que você deseja
+
+// Aula 2
+// var lista = ["maça", "laranja", "uva"]
+// lista.push("pêra") // Adiciona um item no array
+// lista.pop() // Retira o ultimo item do array
+// console.log(lista)
+// console.log(lista.reverse()) // Imprime os itens é em ordem inversa
+// console.log(lista.toString()) // Transforma o array em String
+// console.log(lista.join(" - ")) // Separa os itens como você desejar
+
+// var fruta = {nome:"maça", cor:"vermelha"} // Dicionário com chave e valor
+// console.log(fruta)
+// alert(fruta.cor) // Imprime apenas a cor da fruta
+
+// Aula 3
+// // Condicionais
+// var idade = prompt("Qual sua idade?")
+// if (idade >= 18) {
+//     alert("Maior de idade!")
+// } else {
+//     alert("Menor de idade!")
+// }
+
+// var count = 0
+// while (count <= 5) {
+//     console.log(count)
+//     count += 1
+// }
+
+// var count
+// for (count=0; count <= 5; count++) {
+//     alert(count)
+// }
+
+// // Trabalhando com data
+// var d = new Date();
+// alert(d.getDate()) // Alguns exemplos de manipulações possíveis
+// alert(d.getMinutes())
+// alert(d.getFullYear())
+
+// Aula 4
+// function soma(n1, n2) {
+//     return n1 + n2;
+// }
+
+// function setReplace(frase, nome, novo_nome) {
+//     return frase.replace(nome, novo_nome)
+// }
+
+// alert(soma(5, 10))
+// alert(setReplace("Vai Japão", "Japão", "Palmeiras"))
